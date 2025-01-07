6172a5b720575a045911b3d2097550a2bb3d0586
Oleksii Shtanko <shtankopro@gmail.com>
2025-01-07 23:57:24 +0000

    Add docs + add very good workflow

--- lib/src/lru_cache.dart
+++ lib/src/lru_cache.dart
@@ -2,6 +2,8 @@ import 'dart:collection';
 
 import 'package:synchronized/synchronized.dart';
 
+/// A cache that holds a fixed number of elements and evicts the least
+/// recently used element when full.
 class LruCache<K, V> {
   final LinkedHashMap<K, V> _map;
   final Lock _lock = Lock();
@@ -19,6 +21,10 @@ class LruCache<K, V> {
         _maxSize = maxSize,
         _map = LinkedHashMap<K, V>();
 
+  /// Resizes the cache to the specified [maxSize].
+  /// If the new [maxSize] is smaller than the current size of the cache,
+  /// the least recently used entries will be evicted until the size of the
+  /// cache is less than or equal to the new [maxSize].
   Future<void> resize(int maxSize) async {
     assert(maxSize > 0, 'maxSize must be greater than 0');
     await _lock.synchronized(() {
@@ -27,6 +33,12 @@ class LruCache<K, V> {
     });
   }
 
+  /// Returns the value associated with the [key] or `null` if the [key] is not
+  /// in the cache.
+  /// If the [key] is in the cache, it is marked as the most recently used.
+  /// If the [key] is not in the cache, the [create] method is called to create
+  /// a value for the [key]. If the [create] method returns `null`, `null` is
+  /// returned.
   Future<V?> get(K key) async {
     assert(key != null, 'key must not be null');
     return await _lock.synchronized(() {
@@ -60,6 +72,14 @@ class LruCache<K, V> {
     });
   }
 
+  /// Associates the [key] with the [value] in the cache.
+  /// If the [key] is already in the cache, the [value] is replaced and the
+  /// size of the cache is adjusted.
+  /// If the [key] is not in the cache, the [value] is added and the size of
+  /// the cache is adjusted.
+  /// If the size of the cache exceeds the [maxSize], the least recently used
+  /// entries are evicted until the size of the cache is less than or equal to
+  /// the [maxSize].
   Future<V?> put(K key, V value) async {
     assert(key != null && value != null, 'key and value must not be null');
     return await _lock.synchronized(() {
@@ -80,6 +100,11 @@ class LruCache<K, V> {
     });
   }
 
+  /// Evicts the least recently used entries until the size of the cache is
+  /// less than or equal to the [maxSize].
+  /// If the [maxSize] is less than 0, all entries are evicted.
+  /// This method is called by [put] and [resize] after adding or updating
+  /// an entry.
   Future<void> _trimToSize(int maxSize) async {
     await _lock.synchronized(() {
       while (true) {
@@ -113,6 +138,11 @@ class LruCache<K, V> {
 
   MapEntry<K, V>? _eldest() => _map.entries.firstOrNull;
 
+  /// Removes the [key] and its associated value from the cache.
+  /// Returns the value associated with the [key] or `null` if the [key] is not
+  /// in the cache.
+  /// If the [key] is in the cache, the entry is removed and the size of the
+  /// cache is adjusted.
   Future<V?> remove(K key) async {
     assert(key != null, 'key must not be null');
     return await _lock.synchronized(() {
@@ -129,6 +159,15 @@ class LruCache<K, V> {
 
   V? create(K key) => null;
 
+  /// Returns the size of the [value] for the [key].
+  /// The default implementation returns `1` for all entries.
+  /// Subclasses can override this method to return the actual size of the
+  /// [value].
+  /// The size must be non-negative.
+  /// If the size is negative, a [StateError] is thrown.
+  /// If the size is zero, the entry is evicted from the cache.
+  /// If the size is greater than the [maxSize], the entry is evicted from the
+  /// cache.
   int safeSizeOf(K key, V value) {
     final int result = sizeOf(key, value);
     if (result < 0) {
@@ -137,28 +176,38 @@ class LruCache<K, V> {
     return result;
   }
 
+  /// Returns the size of the [value] for the [key].
   int sizeOf(K key, V value) => 1;
 
+  /// Removes all entries from the cache.
   Future<void> evictAll() async {
     await _trimToSize(-1);
   }
 
+  /// Returns the number of entries in the cache.
   Future<int> size() async {
     return await _lock.synchronized(() => _size);
   }
 
+  /// Returns the maximum size of the cache.
   int maxSize() => _maxSize;
 
+  /// Returns the number of times an entry has been accessed.
   int hitCount() => _hitCount;
 
+  /// Returns the number of times an entry has been accessed.
   int missCount() => _missCount;
 
+  /// Returns the number of times an entry has been created.
   int createCount() => _createCount;
 
+  /// Returns the number of times an entry has been added to the cache.
   int putCount() => _putCount;
 
+  /// Returns the number of times an entry has been evicted.
   int evictionCount() => _evictionCount;
 
+  /// Returns a snapshot of the cache.
   Map<K, V> snapshot() => Map<K, V>.from(_map);
 
   @override
